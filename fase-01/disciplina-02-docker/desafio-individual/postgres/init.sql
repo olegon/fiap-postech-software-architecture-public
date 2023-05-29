fa21e44390c8b4675d1519794484de0e2c1b4649
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2023-05-29 19:41:32 -0300

    Docker - Desafio individual
    - adicionando parâmetro para logar SQL gerado pelo JPA;
    - refatorando script de inicialização do banco de dados;

--- fase-01/disciplina-02-docker/desafio-individual/postgres/init.sql
+++ fase-01/disciplina-02-docker/desafio-individual/postgres/init.sql
@@ -4,21 +4,12 @@ CREATE TABLE programming_languages(
 );
 
 INSERT INTO programming_languages ("name")
-VALUES ('Java');
-
-INSERT INTO programming_languages ("name")
-VALUES ('Kotlin');
-
-INSERT INTO programming_languages ("name")
-VALUES ('C#');
-
-INSERT INTO programming_languages ("name")
-VALUES ('Rust');
-
-INSERT INTO programming_languages ("name")
-VALUES ('C++');
-
-INSERT INTO programming_languages ("name")
-VALUES ('JavaScript');
+VALUES 
+    ('Java'),
+    ('Kotlin'),
+    ('C#'),
+    ('Rust'),
+    ('C++'),
+    ('JavaScript');
 
 \d programming_languages;
